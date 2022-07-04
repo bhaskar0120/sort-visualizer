028c6f6c230d0bc8c871012653ff50475c981e9b
bhaskar0120 <bhaskar0120@gmail.com>
2022-07-04 21:48:18 +0530

    added slider for control

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+
 import { useState } from "react";
 import Navbar from "./navbar";
 
@@ -18,8 +19,20 @@ const FunctionMap  :{[key:string]:JSX.Element} = {
 
 export default function (){
   const [Algo, setAlgo] = useState("insertion");
+  let [number,setNumber] = useState(20);
+  let [speed,setSpeed] = useState(20);
   <Navbar setAlgo={setAlgo} />
   return <>
     {FunctionMap[Algo]}
+    <form>
+      <label>Number of elements</label><br/>
+      <input type="range" value={number} onChange={e=>setNumber(e.target.valueAsNumber)} min={10} max={100} />
+      <label>{number}</label>
+    </form>
+    <form>
+      <label>Speed</label><br/>
+      <input type="range" value={speed} onChange={e=>setNumber(e.target.valueAsNumber)} min={10} max={100} />
+      <label>{speed}</label>
+    </form>
   </>
 }
